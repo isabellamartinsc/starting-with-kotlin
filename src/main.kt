e48e3749ee0fd8aebc50e4784fba38a268eda2ad
Isabella Martins da Cruz <isabellamartinsc@gmail.com>
2020-11-19 14:15:59 -0300

    Testando for loop e while

--- src/main.kt
+++ src/main.kt
@@ -1,25 +1,11 @@
-fun main () {
+fun main() {
+//    calculaSaldo(saldo = 300.0)
+//    testDownTo()
+    testFor()
+}
 
-    var titular = "Isabella"
-    val numeroConta: Int = 1006
-    var saldo: Double = 0.0
-    saldo += 200.0
-    saldo -= 200.0
 
-    println("Bem vindo ao ByteBank")
-    println("Titular da conta: $titular, Número da conta: $numeroConta, Saldo da conta: $saldo")
-
-    //Saldo positivo
-    if (saldo > 0.0){
-        println("Sua contá está com saldo positivo! :)")
-    }
-    //Saldo neutro ou sem saldo
-    else if (saldo == 0.0){
-        println("Você está sem saldo. :|")
-    } else {
-        println("Sua conta está com saldo negativo! :(")
-    }
-
+fun calculaSaldo(saldo: Double) {
     //Saldo positivo
     when {
         saldo > 0.0 -> {
@@ -33,7 +19,53 @@ fun main () {
             println("Sua conta está com saldo negativo! :(")
         }
     }
-
-
 }
 
+fun testFor() {
+    //Cria 5 contas
+    for (i in 1..5) {
+
+        var titular = "Isabella$i"
+        val numeroConta: Int = 1006 + i
+        var saldo: Double = 0.0 + i
+
+        println("Bem vindo ao ByteBank")
+        println("Titular da conta: $titular, Número da conta: $numeroConta, Saldo da conta: $saldo")
+
+        //Saldo positivo
+        if (saldo > 0.0) {
+            println("Sua contá está com saldo positivo! :)")
+        }
+        //Saldo neutro ou sem saldo
+        else if (saldo == 0.0) {
+            println("Você está sem saldo. :|")
+        } else {
+            println("Sua conta está com saldo negativo! :(")
+        }
+    }
+}
+
+fun testDownTo() {
+    //Cria 5 contas em ordem decrescente
+    for (i in 5 downTo 2) {
+
+        var titular = "Isabella$i"
+        val numeroConta: Int = 1006 + i
+        var saldo: Double = 0.0 + i
+
+        println("Bem vindo ao ByteBank")
+        println("Titular da conta: $titular, Número da conta: $numeroConta, Saldo da conta: $saldo")
+
+        //Saldo positivo
+        if (saldo > 0.0) {
+            println("Sua contá está com saldo positivo! :)")
+        }
+        //Saldo neutro ou sem saldo
+        else if (saldo == 0.0) {
+            println("Você está sem saldo. :|")
+        } else {
+            println("Sua conta está com saldo negativo! :(")
+        }
+    }
+
+}
