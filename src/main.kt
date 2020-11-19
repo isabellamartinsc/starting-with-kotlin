bf21b3be3ea035f14c85f2e8c7ee723281e21b66
Isabella Martins da Cruz <isabellamartinsc@gmail.com>
2020-11-19 15:25:59 -0300

    Aprendendo a criar classes

--- src/main.kt
+++ src/main.kt
@@ -1,11 +1,37 @@
 fun main() {
-//    calculaSaldo(saldo = 300.0)
-//    testDownTo()
-//    testFor()
+    //Instanciando a classe de Conta
+    Conta()
 
+    //Instanciando a classe de conta, atribuindo a uma variavel
+
+    val contaIsabella: Conta = Conta()
+    contaIsabella.titular = "Isabella Cruz"
+    contaIsabella.numeroConta = 12345
+    contaIsabella.saldo = 0.00
+
+    val contaEmerson = Conta()
+    contaEmerson.titular = "Emerson Lima"
+    contaEmerson.numeroConta = 54321
+    contaIsabella.saldo = 100.0
+
+    println(contaIsabella.titular)
+    println(contaEmerson.titular)
+
+
+}
+
+//Criada classe de Conta
+class Conta {
+    var titular = ""
+    var numeroConta = 0
+    var saldo = 0.0
+}
+
+
+fun lacosRepeticao() {
     //Loop infinito. Para se incrementarmos a variavel com ++
     var i = 0;
-    while(i < 5){
+    while (i < 5) {
         val titular: String = "Alex $i";
         val numeroConta: Int = 1000 + i;
         var saldo = i + 10.0;
@@ -16,8 +42,56 @@ fun main() {
         println();
         i++;
     }
-}
 
+    fun testFor() {
+        //Cria 5 contas
+        for (i in 1..5) {
+
+            var titular = "Isabella$i"
+            val numeroConta: Int = 1006 + i
+            var saldo: Double = 0.0 + i
+
+            println("Bem vindo ao ByteBank")
+            println("Titular da conta: $titular, Número da conta: $numeroConta, Saldo da conta: $saldo")
+
+            //Saldo positivo
+            if (saldo > 0.0) {
+                println("Sua contá está com saldo positivo! :)")
+            }
+            //Saldo neutro ou sem saldo
+            else if (saldo == 0.0) {
+                println("Você está sem saldo. :|")
+            } else {
+                println("Sua conta está com saldo negativo! :(")
+            }
+        }
+    }
+
+    fun testDownTo() {
+        //Cria 5 contas em ordem decrescente
+        for (i in 5 downTo 2) {
+
+            var titular = "Isabella$i"
+            val numeroConta: Int = 1006 + i
+            var saldo: Double = 0.0 + i
+
+            println("Bem vindo ao ByteBank")
+            println("Titular da conta: $titular, Número da conta: $numeroConta, Saldo da conta: $saldo")
+
+            //Saldo positivo
+            if (saldo > 0.0) {
+                println("Sua contá está com saldo positivo! :)")
+            }
+            //Saldo neutro ou sem saldo
+            else if (saldo == 0.0) {
+                println("Você está sem saldo. :|")
+            } else {
+                println("Sua conta está com saldo negativo! :(")
+            }
+        }
+
+    }
+}
 
 fun calculaSaldo(saldo: Double) {
     //Saldo positivo
@@ -35,51 +109,4 @@ fun calculaSaldo(saldo: Double) {
     }
 }
 
-fun testFor() {
-    //Cria 5 contas
-    for (i in 1..5) {
 
-        var titular = "Isabella$i"
-        val numeroConta: Int = 1006 + i
-        var saldo: Double = 0.0 + i
-
-        println("Bem vindo ao ByteBank")
-        println("Titular da conta: $titular, Número da conta: $numeroConta, Saldo da conta: $saldo")
-
-        //Saldo positivo
-        if (saldo > 0.0) {
-            println("Sua contá está com saldo positivo! :)")
-        }
-        //Saldo neutro ou sem saldo
-        else if (saldo == 0.0) {
-            println("Você está sem saldo. :|")
-        } else {
-            println("Sua conta está com saldo negativo! :(")
-        }
-    }
-}
-
-fun testDownTo() {
-    //Cria 5 contas em ordem decrescente
-    for (i in 5 downTo 2) {
-
-        var titular = "Isabella$i"
-        val numeroConta: Int = 1006 + i
-        var saldo: Double = 0.0 + i
-
-        println("Bem vindo ao ByteBank")
-        println("Titular da conta: $titular, Número da conta: $numeroConta, Saldo da conta: $saldo")
-
-        //Saldo positivo
-        if (saldo > 0.0) {
-            println("Sua contá está com saldo positivo! :)")
-        }
-        //Saldo neutro ou sem saldo
-        else if (saldo == 0.0) {
-            println("Você está sem saldo. :|")
-        } else {
-            println("Sua conta está com saldo negativo! :(")
-        }
-    }
-
-}
